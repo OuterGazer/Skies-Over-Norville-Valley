756de2d08ef8989712827c15f6a11b29f5d74b86
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-08-26 11:12:02 +0200

    Installed package for new input system. Created player movement script.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] InputAction playerMov;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnEnable()
+    {
+        this.playerMov.Enable();
+    }
+
+    private void OnDisable()
+    {
+        this.playerMov.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalThrow = this.playerMov.ReadValue<Vector2>().x;
+        float verticalThrow = this.playerMov.ReadValue<Vector2>().y;
+
+        Debug.Log(horizontalThrow);
+        Debug.Log(verticalThrow);
     }
 }
