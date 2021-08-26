2a80fa988f78351fd2e58461f539c4961370fece
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-08-26 15:07:42 +0200

    Clamped player movement in the X axis using the camera's field of view and display resolution of the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,34 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] InputAction playerMov;
     [SerializeField] float moveSpeed = default;
+    [Range(0, 1)][SerializeField] float xScreenLimitPercent = default;
+
+    private float xScreenLimit;
+    private float yScreenLimit;
+
+    private Camera playerView;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.playerView = Camera.main;
+
+        // To get the real horizontal screen space we apply som trigonometry. We multiply the camera distance to the player times the tangent of half of the horizontal field of view angle
+        this.xScreenLimit = Mathf.Abs(this.playerView.transform.localPosition.z) *
+                            Mathf.Tan(Mathf.Deg2Rad * 
+                                      (Camera.VerticalToHorizontalFieldOfView(this.playerView.fieldOfView, this.playerView.aspect) / 2));
+        this.xScreenLimit /= 1_000; // To convert from absolute pixel measurement to screen measurement
+        this.xScreenLimit *= this.xScreenLimitPercent; 
+
+        Debug.Log(this.xScreenLimit);
+
+        /*this.xScreenLimit = Screen.width;
+        this.yScreenLimit = Screen.height;*/
+
+        /*this.xScreenLimit = Display.main.renderingWidth;
+        this.yScreenLimit = Display.main.renderingHeight;*/
+        /*Debug.Log(this.xScreenLimit);
+        Debug.Log(this.yScreenLimit);*/
     }
 
     private void OnEnable()
@@ -33,10 +56,21 @@ public class PlayerMovement : MonoBehaviour
         float xPosNextFrame = xThrow * this.moveSpeed * Time.deltaTime;
         float yPosNextFrame = yThrow * this.moveSpeed * Time.deltaTime;
 
-        this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x + xPosNextFrame,
+        Vector3 nextPlayerPos = new Vector3(this.gameObject.transform.localPosition.x + xPosNextFrame,
+                                            this.gameObject.transform.localPosition.y + yPosNextFrame,
+                                            this.gameObject.transform.localPosition.z);
+
+        Vector3 playerPos = this.playerView.ScreenToViewportPoint(nextPlayerPos);
+
+        playerPos.x = Mathf.Clamp(playerPos.x, -this.xScreenLimit, this.xScreenLimit);
+
+        nextPlayerPos = this.playerView.ViewportToScreenPoint(playerPos);
+
+        this.gameObject.transform.localPosition = new Vector3(nextPlayerPos.x,
                                                               this.gameObject.transform.localPosition.y + yPosNextFrame,
                                                               this.gameObject.transform.localPosition.z);
 
-
+        Debug.Log(playerPos.x + " x position");
+        Debug.Log(playerPos.y + " y position");
     }
 }
