47f76c451c0d9cc5db9fa8bdbf56d2b81457f921
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-08-26 10:21:23 +0200

    Tweaked terrain, tweaked timeline animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public int testVariable = 56;
-
     // Start is called before the first frame update
     void Start()
     {
