628ae772eee7cc3cc21f660a7285a799a15cf75d
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-08-27 11:33:31 +0200

    Implemented crosshair and uploaded a png of the terrain

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -27,6 +27,9 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         CalculateScreenLimits();
     }
 
