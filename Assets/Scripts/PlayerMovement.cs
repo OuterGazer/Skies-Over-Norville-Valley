ab88e7c72aa2872b9b1ee804cb3a66db16c8a5da
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-08-27 11:09:55 +0200

    Implemented airship rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Settings")]
     [SerializeField] InputAction playerMov;
     [SerializeField] float moveSpeed = default;
+    [SerializeField] float rotatingSpeed = default;
+    [SerializeField] float positionPitchFactor = default;
+    [SerializeField] float controlPitchFactor = default;
+    [SerializeField] float positionYawFactor = default;
+    [SerializeField] float controlRollFactor = default;
+
+    [Header("Camera Settings")]
     [Range(0, 1)][SerializeField] float xScreenLimitPercent = default;
     [Range(0, 1)] [SerializeField] float yScreenLimitPercent = default;
 
+    float xThrow, yThrow;
     private float xScreenLimit;
     private float yScreenLimit;
 
@@ -18,18 +27,23 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        CalculateScreenLimits();
+    }
+
+    private void CalculateScreenLimits()
+    {
         this.playerView = Camera.main;
 
-        // To get the real horizontal screen space we apply som trigonometry. We multiply the camera distance to the player times the tangent of half of the horizontal field of view angle
+        // To get the real horizontal screen space we apply some trigonometry. We multiply the camera distance to the player times the tangent of half of the horizontal field of view angle
         this.xScreenLimit = Mathf.Abs(this.playerView.transform.localPosition.z) *
-                            Mathf.Tan(Mathf.Deg2Rad * 
+                            Mathf.Tan(Mathf.Deg2Rad *
                                       (Camera.VerticalToHorizontalFieldOfView(this.playerView.fieldOfView, this.playerView.aspect) / 2));
         this.xScreenLimit /= 1_000; // To convert from absolute pixel measurement to screen measurement
-        this.xScreenLimit *= this.xScreenLimitPercent; 
+        this.xScreenLimit *= this.xScreenLimitPercent; // To avoid half of the airplane to disappear from the screen, due to the pivot position.
 
         this.yScreenLimit = Mathf.Abs(this.playerView.transform.localPosition.z) *
                             Mathf.Tan(Mathf.Deg2Rad * this.playerView.fieldOfView / 2);
-        this.yScreenLimit /= 1_000; // To convert from absolute pixel measurement to screen measurement
+        this.yScreenLimit /= 1_000;
         this.yScreenLimit *= this.yScreenLimitPercent;
     }
 
@@ -46,10 +60,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow, yThrow;
-        ReadPlayerInput(out xThrow, out yThrow);
+        ProcessPlayerMovement();
 
-        Vector3 nextPlayerPos = CalculateNextPlayerPosition(xThrow, yThrow);
+        ProcessPlayerRotation();
+    }
+
+    private void ProcessPlayerRotation()
+    {
+        float xPitch, yYaw, zRoll;
+
+        xPitch = this.gameObject.transform.localPosition.y * this.positionPitchFactor + // To have the ship not always face the center of the screen, but be more or less straight in relation to its local position
+                 this.yThrow * this.controlPitchFactor; // Rotates up/down according to player input
+        yYaw = this.gameObject.transform.localPosition.x * this.positionYawFactor; // Same, moving sideways we apply a slight yaw that keeps the airship straight, as it automatically rotates to face the relative center of the view
+        zRoll = this.xThrow * this.controlRollFactor;
+
+        this.gameObject.transform.localRotation = Quaternion.RotateTowards(this.gameObject.transform.localRotation,
+                                                                           Quaternion.Euler(xPitch, yYaw, zRoll),
+                                                                           this.rotatingSpeed * Time.deltaTime);
+    }
+
+    private void ProcessPlayerMovement()
+    {
+        
+        ReadPlayerInput();
+
+        Vector3 nextPlayerPos = CalculateNextPlayerPosition(this.xThrow, this.yThrow);
 
         Vector3 playerPos = this.playerView.ScreenToViewportPoint(nextPlayerPos); // Convert player position into camera viewport units
 
@@ -58,12 +93,12 @@ public class PlayerMovement : MonoBehaviour
         nextPlayerPos = this.playerView.ViewportToScreenPoint(playerPos); // Convert from viewport units back into world units
 
         MovePlayer(nextPlayerPos);
-    }    
+    }
 
-    private void ReadPlayerInput(out float xThrow, out float yThrow)
+    private void ReadPlayerInput()
     {
-        xThrow = this.playerMov.ReadValue<Vector2>().x;
-        yThrow = this.playerMov.ReadValue<Vector2>().y;
+        this.xThrow = this.playerMov.ReadValue<Vector2>().x;
+        this.yThrow = this.playerMov.ReadValue<Vector2>().y;
     }
 
     private Vector3 CalculateNextPlayerPosition(float xThrow, float yThrow)
