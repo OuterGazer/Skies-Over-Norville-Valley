18e5b88d8f3b02cd3c3d714fc5d9dd7aae6bc0f2
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-08-30 16:05:00 +0200

    Fixed cached amunition system

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,19 +4,50 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] float bulletSpeed = default;
+    [SerializeField] private float bulletSpeed = default;
+    [SerializeField] private float shootRange = default;
 
-    Rigidbody bulletRB;
+    private float originZ;
 
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody bulletRB;
+    private TrailRenderer bulletTrail;
+    public void EmmitTrail(bool shouldEmmit)
     {
-        this.bulletRB = this.gameObject.GetComponent<Rigidbody>();
+        this.bulletTrail.emitting = shouldEmmit;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        this.bulletRB = this.gameObject.GetComponent<Rigidbody>();
+        this.bulletTrail = this.gameObject.GetComponent<TrailRenderer>();
+        //this.gameObject.GetComponent<TrailRenderer>().enabled = false;
+    }
+
+    private void FixedUpdate()
     {
         this.bulletRB.MovePosition(this.bulletRB.position + this.gameObject.transform.forward * this.bulletSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Update()
+    {
+        if(Mathf.Abs(this.gameObject.transform.position.z - this.originZ) >= this.shootRange)
+        {
+            SetParent(GameObject.FindWithTag("Ammo Holder").transform);            
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void DisengageFromParent()
+    {
+        this.gameObject.transform.SetParent(null);
+        this.originZ = Mathf.Abs(this.gameObject.transform.position.z);
+    }
+
+    public void SetParent(Transform parent)
+    {
+        EmmitTrail(false);
+        this.gameObject.transform.position = Vector3.zero;        
+        this.gameObject.transform.SetParent(parent);        
     }
 }
--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -6,15 +6,24 @@ using UnityEngine.InputSystem;
 public class PlayerShooting : MonoBehaviour
 {
     [Header("Shooting Settings")]
-    [SerializeField] InputAction playerShooting;
-    [SerializeField] Transform ammoParent;
-    [SerializeField] GameObject rightBarrel;
-    [SerializeField] GameObject leftBarrel;
-    [SerializeField] Bullet shootingAmmo;
-    [SerializeField] int maxAmmo = default;
+    [SerializeField] private InputAction playerShooting;
+    [SerializeField] private Transform ammoParent;
+    [SerializeField] private GameObject rightBarrel;
+    [SerializeField] private GameObject leftBarrel;
+    [SerializeField] private Bullet shootingAmmo;
+    [SerializeField] private int maxAmmo = default;
+    [SerializeField] private float bulletBarrageFactor = default; // For debugging purposes only
 
-    Bullet[] airshipAmmo;
-    int currentBullet = 0;
+    
+    private Bullet[] airshipAmmo;
+    private int currentBullet = 0;
+    private float fireNextBullet;
+    private void SetFireNextBullet()
+    {
+        this.fireNextBullet = this.bulletBarrageFactor;
+    }
+
+
 
     private void OnEnable()
     {
@@ -35,20 +44,34 @@ public class PlayerShooting : MonoBehaviour
             airshipAmmo[i] = GameObject.Instantiate<Bullet>(this.shootingAmmo, this.ammoParent);
             airshipAmmo[i].gameObject.SetActive(false);
         }
+
+        SetFireNextBullet();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (this.playerShooting.triggered)
+        this.fireNextBullet -= Time.deltaTime;
+
+        if((this.playerShooting.ReadValue<float>() != 0) &&
+           (this.fireNextBullet < 0))
         {
             this.airshipAmmo[this.currentBullet].gameObject.SetActive(true);
+            //this.airshipAmmo[this.currentBullet].EmmitTrail(false);
+
             this.airshipAmmo[this.currentBullet].gameObject.transform.localPosition = this.rightBarrel.transform.localPosition;
             this.airshipAmmo[this.currentBullet].gameObject.transform.localRotation = this.rightBarrel.transform.localRotation;
-            this.currentBullet++;
+            this.airshipAmmo[this.currentBullet].EmmitTrail(true);
 
-            if (this.currentBullet > this.airshipAmmo.Length)
-                this.currentBullet = 0;
+            this.airshipAmmo[this.currentBullet].DisengageFromParent();
+
+            SetFireNextBullet();
+            this.currentBullet++;            
         }
+
+        if (this.currentBullet >= this.airshipAmmo.Length)
+            this.currentBullet = 0;
     }
+
+    
 }
