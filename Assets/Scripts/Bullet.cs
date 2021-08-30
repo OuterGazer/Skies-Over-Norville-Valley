b93b6797167e5b4b1d070649654bc028cd27af3c
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-08-30 12:07:19 +0200

    Fixed bullet spawning and forward movement

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] float bulletSpeed = default;
+
+    Rigidbody bulletRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.bulletRB = this.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        this.bulletRB.MovePosition(this.bulletRB.position + this.gameObject.transform.forward * this.bulletSpeed * Time.fixedDeltaTime);
     }
 }
--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -7,7 +7,14 @@ public class PlayerShooting : MonoBehaviour
 {
     [Header("Shooting Settings")]
     [SerializeField] InputAction playerShooting;
+    [SerializeField] Transform ammoParent;
+    [SerializeField] GameObject rightBarrel;
+    [SerializeField] GameObject leftBarrel;
     [SerializeField] Bullet shootingAmmo;
+    [SerializeField] int maxAmmo = default;
+
+    Bullet[] airshipAmmo;
+    int currentBullet = 0;
 
     private void OnEnable()
     {
@@ -21,7 +28,13 @@ public class PlayerShooting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.airshipAmmo = new Bullet[this.maxAmmo];
+
+        for(int i = 0; i < airshipAmmo.Length; i++)
+        {
+            airshipAmmo[i] = GameObject.Instantiate<Bullet>(this.shootingAmmo, this.ammoParent);
+            airshipAmmo[i].gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -29,7 +42,13 @@ public class PlayerShooting : MonoBehaviour
     {
         if (this.playerShooting.triggered)
         {
+            this.airshipAmmo[this.currentBullet].gameObject.SetActive(true);
+            this.airshipAmmo[this.currentBullet].gameObject.transform.localPosition = this.rightBarrel.transform.localPosition;
+            this.airshipAmmo[this.currentBullet].gameObject.transform.localRotation = this.rightBarrel.transform.localRotation;
+            this.currentBullet++;
 
+            if (this.currentBullet > this.airshipAmmo.Length)
+                this.currentBullet = 0;
         }
     }
 }
