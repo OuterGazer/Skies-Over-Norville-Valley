032993e3ddb3e923033ca823f0af8ac7e2c37b5b
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-09-13 10:01:15 +0200

    Implemented tank bullet shooting.

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    [SerializeField] private float speed = default;
+    
+    private float range = default;
+
+
+    private Rigidbody bulletRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.bulletRB = this.gameObject.GetComponent<Rigidbody>();
+
+        this.range = GameObject.FindObjectOfType<CollisionHandler>().transform.position.y + 10f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.bulletRB.MovePosition(this.bulletRB.position + this.gameObject.transform.forward * this.speed * Time.fixedDeltaTime);
+
+        if (this.gameObject.transform.position.y >= this.range)
+            GameObject.Destroy(this.gameObject);
+    }
+
+    /*public void AlignBulletWithCannon(Vector3 cannonForwardDir)
+    {
+        this.gameObject.transform.forward = cannonForwardDir;
+    }*/
+}
---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    [SerializeField] EnemyBullet bullet;
+    [SerializeField] float timeBetweenBullets = default;
+    [SerializeField] float barrageBulletAmount = default;
+    [SerializeField] float timeBetweenBarrages = default;
+
+
+    private float bulletTimeCounter;
+    private float bulletCounter;
+    private float barrageTimeCounter;
+
+    private bool waitForNextBarrage = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.bulletTimeCounter = this.timeBetweenBullets;
+        this.barrageTimeCounter = this.timeBetweenBarrages;
+    }
+
+    private void FixedUpdate()
+    {
+        SpawnBullets();
+    }
+
+    private void SpawnBullets()
+    {
+        this.bulletTimeCounter -= Time.fixedDeltaTime;
+
+        if ((this.bulletTimeCounter <= 0) && (this.bulletCounter <= this.barrageBulletAmount) &&
+            (!this.waitForNextBarrage))
+        {
+            EnemyBullet bulletShot = Instantiate<EnemyBullet>(this.bullet, this.gameObject.transform.position, this.gameObject.transform.rotation);
+            this.bulletTimeCounter = this.timeBetweenBullets;
+            this.bulletCounter++;
+
+           SetBulletDirection(bulletShot);
+
+            if (this.bulletCounter >= this.barrageBulletAmount)
+            {
+                this.bulletCounter = 0;
+
+                this.waitForNextBarrage = true;
+            }
+        }
+
+        StopBulletsBetweenBarrages();
+    }
+
+    private void SetBulletDirection(EnemyBullet bulletShot)
+    {
+        switch (this.gameObject.tag)
+        {
+            case "Left Turret":
+                bulletShot.transform.Rotate(Vector3.up, -90f);
+                break;
+        }
+    }
+
+    private void StopBulletsBetweenBarrages()
+    {
+        if (this.waitForNextBarrage)
+        {
+            this.barrageTimeCounter -= Time.fixedDeltaTime;
+
+            if (this.barrageTimeCounter <= 0)
+            {
+                this.waitForNextBarrage = false;
+                this.barrageTimeCounter = this.timeBetweenBarrages;
+            }
+
+        }
+    }
+}
