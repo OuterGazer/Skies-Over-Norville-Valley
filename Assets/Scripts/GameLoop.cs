082513193ca6c2e27dd78edc8329f06d8ce79f7c
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2021-09-17 10:59:31 +0200

    Implemented controls for pausing, quitting and enabling/disabling the crosshair.

--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.InputSystem;
 
 public class GameLoop : MonoBehaviour
 {
     private PlayableDirector timeline;
+    [SerializeField] InputAction enableDisableCrosshair;
+    [SerializeField] SpriteRenderer crosshair;
+    [SerializeField] InputAction pause;
+    [SerializeField] InputAction quit; 
 
     private bool firstTeleport = false;
+
+    private void OnEnable()
+    {
+        this.enableDisableCrosshair.Enable();
+        this.pause.Enable();
+        this.quit.Enable();
+    }
+    private void OnDisable()
+    {
+        this.enableDisableCrosshair.Disable();
+        this.pause.Disable();
+        this.quit.Disable();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +37,8 @@ public class GameLoop : MonoBehaviour
     void Update()
     {
         LoopBackToBeginning();
+
+        ProcessControls();
     }
 
     private void LoopBackToBeginning()
@@ -33,5 +53,22 @@ public class GameLoop : MonoBehaviour
         {
             this.timeline.time = 7.33;
         }
+    }
+
+    private void ProcessControls()
+    {
+        if (this.enableDisableCrosshair.triggered)
+            this.crosshair.enabled = !this.crosshair.enabled;
+
+        if (this.pause.triggered)
+        {
+            if (Time.timeScale != 0)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+        }
+
+        if (this.quit.triggered)
+            Application.Quit();
     }
 }
